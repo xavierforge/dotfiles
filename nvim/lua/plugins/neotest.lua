3a61400cdc6e9848f69c070039ea940068892a9e
ChihYing <spacetime0311@gmail.com>
2026-04-23 22:47:08 +0800

    feat(nvim): add neotest jump keymaps (]n/[n and failed-only variants)

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -9,5 +9,28 @@ return {
         ["neotest-vitest"] = {},
       },
     },
+    -- 測試跳轉快捷鍵（]n/[n 下一個/上一個；大寫 N 版本只跳「失敗的」測試）
+    keys = {
+      {
+        "]n",
+        function() require("neotest").jump.next() end,
+        desc = "Next Test",
+      },
+      {
+        "[n",
+        function() require("neotest").jump.prev() end,
+        desc = "Prev Test",
+      },
+      {
+        "]N",
+        function() require("neotest").jump.next({ status = "failed" }) end,
+        desc = "Next Failed Test",
+      },
+      {
+        "[N",
+        function() require("neotest").jump.prev({ status = "failed" }) end,
+        desc = "Prev Failed Test",
+      },
+    },
   },
 }
