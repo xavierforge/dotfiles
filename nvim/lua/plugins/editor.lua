2c1981d54b88398c71ac1c73721316491535ca51
Xavier Yen <xavier@xavierforge.dev>
2026-07-01 21:48:25 +0800

    feat(nvim): show hidden files in neo-tree by default

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -5,4 +5,16 @@ return {
   -- 用 Ctrl+hjkl 無縫切換 vim 視窗與 tmux 窗格
   -- Seamlessly move between vim splits and tmux panes with Ctrl+hjkl
   { "christoomey/vim-tmux-navigator", event = { "BufReadPre" } },
+  {
+    -- 讓 neo-tree 預設就顯示隱藏檔（等同一開啟就按了 H）
+    -- Make neo-tree show hidden files by default (as if you'd pressed H)
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true, -- 顯示被過濾的項目（點檔、gitignore 檔）/ Show filtered items (dotfiles, gitignored)
+        },
+      },
+    },
+  },
 }
