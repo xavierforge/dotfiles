f94a796540c1141c85c4ab967b66323ad2739524
Xavier Yen <xavier@xavierforge.dev>
2026-07-01 20:37:27 +0800

    fix(nvim): use catppuccin-nvim colorscheme name

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -20,7 +20,7 @@ require("lazy").setup({
     {
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
-      opts = { colorscheme = "catppuccin" },
+      opts = { colorscheme = "catppuccin-nvim" },
     },
     -- import/override with your plugins
     -- LSP
@@ -54,7 +54,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "catppuccin", "habamax" } },
+  install = { colorscheme = { "catppuccin-nvim", "habamax" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
